6f2ef6b91d0b0b710c610e4a48aa08591f27f78e
huypfui <xuanduydanh@gmail.com>
2023-03-10 10:41:15 +0100

    merge to pull MovieItem update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 
 // Library import
+import { useState, useEffect } from "react"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 // Component import
@@ -12,6 +13,19 @@ import MovieDetails from "./pages/MovieDetails"
 
 
 function App() {
+  const [trendMovie, setTrendMovie] = useState([])
+  const apiKey = "2f42e4a86b0ac5a0f11b8f51ca045ce0"
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`)
+      .then(response => response.json())
+      .then(data => {
+        setTrendMovie(data)
+      })
+  }, [])
+
+  console.log(trendMovie)
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -19,8 +33,9 @@ function App() {
         <Route path="/" element={<IntroApp/>}/>
         <Route path="/home" element = {<Home/>}/>
         <Route path ="/search" element = {<GenresSearch/>}/>
-        <Route path ="/search/:type" element = {<GenresSearch/>}/>
+        <Route path ="/moviedetails/:trending" element = {<GenresSearch/>}/>
         <Route path ="/moviedetails/:name" element={<MovieDetails/>}/>
+        
 
       </Routes>
       </BrowserRouter>
--- src/components/movieItem/MovieItem.js
+++ src/components/movieItem/MovieItem.js
@@ -15,7 +15,6 @@ const MovieItem = (props) => {
     </article>
 
 
-
     );
 }
 
