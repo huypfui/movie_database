025598e809e299b0a753d51ff46030f1e42d30f0
Hanni <ngynngchn@gmail.com>
2023-03-11 12:45:15 +0100

    added functionality to show colored home on home page

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,27 +1,34 @@
 // CSS import
-import "./Navigation.scss"
+import "./Navigation.scss";
 
+// Component import
 import React from "react";
 import { Link } from "react-router-dom";
 
+// Image import
+import homeGrey from "../../img/home_grey.svg";
+import homeRed from "../../img/home_red.svg";
+import bookmark from "../../img/bookmark.svg";
+import mailbox from "../../img/mailbox.svg";
+import profile from "../../img/profile.svg";
 
-
-import homeButton from "../../img/home_grey.svg"
-import bookmark from "../../img/bookmark.svg"
-import mailbox from "../../img/mailbox.svg"
-import profile from "../../img/profile.svg"
-
-const Navigation = () => {
-    return (
-    <div className="navBar">
-        <Link to="/Home">
-            <img src={homeButton} alt="Home Button"/>
-        </Link>
-        <img src={bookmark} alt="Bookmark"/>
-        <img src={mailbox} alt="Mailbox"/>
-        <img src={profile} alt="Profile"/>
-    </div>
-    );
-}
+const Navigation = ({ home }) => {
+	return (
+		<div className="navBar">
+			{home ? (
+				<Link to="/Home">
+					<img src={homeRed} alt="Home Button" />
+				</Link>
+			) : (
+				<Link to="/Home">
+					<img src={homeGrey} alt="Home Button" />
+				</Link>
+			)}
+			<img src={bookmark} alt="Bookmark" />
+			<img src={mailbox} alt="Mailbox" />
+			<img src={profile} alt="Profile" />
+		</div>
+	);
+};
 
 export default Navigation;
