bb3f5cb34cd123b3a04ecd8d94154f04ea6c4474
Hanni <ngynngchn@gmail.com>
2023-03-14 12:52:21 +0100

    replaced mockup with actual

--- src/pages/IntroApp.js
+++ src/pages/IntroApp.js
@@ -1,32 +1,41 @@
 // import library
-import {useState} from "react"
-import SplashScreen from "../components/splashscreen/Splashscreen"
-import {Link} from 'react-router-dom'
+import { useState } from "react";
+import SplashScreen from "../components/splashscreen/Splashscreen";
+import { Link } from "react-router-dom";
 
-import "./IntroApp.scss"
+import "./IntroApp.scss";
 
-import placeholder from "../img/introMockup.svg"
-
+import placeholder from "../img/introMockup.svg";
 
 const IntroApp = () => {
-    const [show, setShow] = useState(true) 
-    const handleClick = () => {
-        setShow(false)    
-    } 
+	const [show, setShow] = useState(true);
+	const handleClick = () => {
+		setShow(false);
+	};
 
+	return (
+		<div className="introApp">
+			<img src={placeholder} alt="Beispielbild" />
+			<div className="intro">
+				<h2>
+					Enjoy Your Movie
+					<br />
+					Watch Everywhere
+				</h2>
+				<p>
+					Stream unlimited movies and TV shows
+					<br />
+					on your phone, tablet, laptop and TV.
+				</p>
+				<br />
+				<Link className="link" to="/Home">
+					Get Started
+				</Link>
+			</div>
 
-    return ( 
-    <div className="introApp">
-        <img src={placeholder} alt="Beispielbild" />
-        <div className="intro">
-            <h2>Enjoy Your Movie<br/>Watch Everywhere</h2>
-            <p>Stream unlimited movies and TV shows<br/>on your phone,tablet, laptop, and TV.</p><br/>
-            <Link className="link" to="/Home">Get Started</Link>
-        </div>
-
-        { show ? <SplashScreen handleClick={handleClick}/>  : "" }
-
-    </div> );
-}
+			{show ? <SplashScreen handleClick={handleClick} /> : ""}
+		</div>
+	);
+};
 
 export default IntroApp;
